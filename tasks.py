f7d5efa558eb5a72462430efe1cc0631b35231d1
Chris <chris.lyon.tech@gmail.com>
2015-12-01 20:54:52 +0100

    Ajout de tasks

--- tasks.py
+++ tasks.py
@@ -5,7 +5,13 @@ def hello():
 	print("Hello World")
 
 @task
-def update_git():
-	run('rm data.db')
-	run('find -name __pycache__ -exec rm {} \;')
-	run('git status')
+def purge():
+	run('rm -f data.db')
+	run("find . -name '__pycache__' -exec rm -fr {} \;")
+	#run('git status')
+
+@task
+def test():
+	run('rm -f data.db')
+	run('python definitions.py')
+	run('python populate.py')
