292748ce4359034783d541d65dab9687ea3ebbc0
Chris <chris.lyon.tech@gmail.com>
2015-12-02 12:15:22 +0100

    debut dev

---
+++ db_activity/client.py
@@ -0,0 +1,68 @@
+## --------------------
+## Prise de commande
+## --------------------
+
+#Etape 1 : init + connexion
+#Etape 2 : Recup du catalogue
+#Etape 3 : Choix du client 
+#Etape 4 : Generation de commande
+
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
+ 
+from definitions import Base, BASE_NAME
+from definitions import Client, Produit, Stock, ComCli, LigCli
+
+import datetime
+import random
+
+## Connexion
+def connect():
+	engine = create_engine(BASE_NAME, echo=False)
+	Base.metadata.bind = engine
+	DBSession = sessionmaker(bind=engine)
+	session = DBSession()
+	return session
+
+def get_catalog():
+	session = connect()
+	s = select([Produit.id])
+	prods = session.execute(s)
+	p = [ x[0] for x in prods]
+	session.close()
+	return p
+
+def get_client():
+	session = connect()
+	s = select([Client.id])
+	clis = session.execute(s)
+	l = [ x[0] for x in clis]
+	session.close()
+	return random.choice(l)
+
+def get_prods(cat):
+	max_cmd = len(cat)
+	nb_pro_cmd = random.randint(0, max_cmd-1)+1
+	print("Nb Produits a commandes : %s " % nb_pro_cmd )
+	pros = random.sample(cat, nb_pro_cmd)
+	print("Produits commandes : %s " % pros )
+
+def gnr_comcli(cli, prods):
+	pass
+
+
+def run():
+	#Get catalog
+	catalog = get_catalog()
+	print(catalog)
+	#choix du client
+	client = get_client()
+	print(client)
+	# Choix du catalog
+	prods_cmd = get_prods(catalog)
+	#Gnr_cmdcli
+	gnr_comcli(client, prods_cmd)
+ 
+
+if __name__ == '__main__':
+	run()
--- db_activity/definitions.py
+++ db_activity/definitions.py
@@ -5,7 +5,7 @@
 import os
 import sys
 from sqlalchemy import Column, ForeignKey
-from sqlalchemy import Integer, String, DateTime, Date, Numeric
+from sqlalchemy import Integer, String, DateTime, Date, Numeric, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -16,25 +16,31 @@ Base = declarative_base()
 BASE_NAME = 'sqlite:///data.db'
 
 class BASE_TABLE():
+	" Classe commune a toute les autres"
 	id = Column(Integer, primary_key=True)
 	d_cre = Column(DateTime, default = datetime.datetime.now() )
 	d_mod = Column(DateTime, default = datetime.datetime.now(), onupdate=datetime.datetime.now() )
 
 class Client(Base, BASE_TABLE):
+	"Le fichier client"
 	__tablename__ = 'CLIENTS'
 	nom = Column(String(20), nullable=False, unique=True)
 
 class Produit(Base, BASE_TABLE):
+	"Les produits"
 	__tablename__ = 'PRODUITS'
 	desig = Column(String(30), nullable=False, unique=True)
-	prix = Column(Numeric(12,2))
+	#prix = Column(Numeric(12,2))
+	prix = Column(BigInteger)
 
 class Fourn(Base, BASE_TABLE):
+	"Les fournisseurs"
 	__tablename__ = 'FOURN'
 	nom = Column(String(20), nullable=False, unique=True)
 	delai = Column(Integer)
 
 class Stock(Base, BASE_TABLE):
+	"Le stock par depot"
 	__tablename__ = 'STOCK'
 	depot = Column(String(10), nullable=False)
 	produit_id = Column(Integer, ForeignKey('PRODUITS.id'))
@@ -42,6 +48,7 @@ class Stock(Base, BASE_TABLE):
 	qstock = Column(Integer)
 
 class ComCli(Base, BASE_TABLE):
+	"Les commandes clients"
 	__tablename__ = 'COMCLI'
 	numcom = Column(Integer, unique=True)
 	datcom = Column(DateTime, default = datetime.datetime.now() )
@@ -50,17 +57,20 @@ class ComCli(Base, BASE_TABLE):
 	facture = Column(Integer)
 
 class LigCli(Base, BASE_TABLE):
+	"Les lignes de commandes clients"
 	__tablename__ = 'LIGCLI'
 	numcom = Column(Integer)
 	numlig = Column(Integer)
 	produit_id = Column(Integer, ForeignKey('PRODUITS.id'))
 	produit = relationship(Produit)
 	qcom = Column(Integer)
-	prix = Column(Numeric(12,2))
+	#prix = Column(Numeric(12,2))
+	prix = Column(BigInteger)
 	qliv = Column(Integer)
 	qfac = Column(Integer)
 
 class LigFou(Base, BASE_TABLE):
+	"Les commandes fournissuers"
 	__tablename__ = 'LIGFOU'
 	numcom = Column(Integer, unique=True)
 	numlig = Column(Integer)
@@ -69,7 +79,8 @@ class LigFou(Base, BASE_TABLE):
 	produit_id = Column(Integer, ForeignKey('PRODUITS.id'))
 	produit = relationship(Produit)
 	qcom = Column(Integer)
-	prix = Column(Numeric(12,2))
+	#prix = Column(Numeric(12,2))
+	prix = Column(BigInteger)
 	dprevu = Column(DateTime)
 	qrecu = Column(Integer)
 	
